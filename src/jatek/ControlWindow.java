903505f14284d2c18bc286945658103220b66e03
mayad <mayadam77@gmail.com>
2022-11-26 13:46:24 +0100

    11.26.13.46.

--- src/jatek/ControlWindow.java
+++ src/jatek/ControlWindow.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 
 /**
