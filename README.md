2dda6dc9988643ea896c34fdd1d7425b4948550f
mayad <mayadam77@gmail.com>
2022-11-27 15:06:09 +0100

    11.26.15.05.

--- README.md
+++
@@ -1,2 +0,0 @@
-# GoLtest
-game of life prog3 nhf 22.11.26.
--- src/jatek/LifeMain.java
+++
@@ -1,7 +0,0 @@
-package jatek;
-
-public class LifeMain {
-    public static void main(String[] args) {
-        ControlWindow jatek = new ControlWindow();
-    }
-}
